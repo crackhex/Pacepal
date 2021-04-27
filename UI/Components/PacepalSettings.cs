1a4adbd8a8cad4418994421614ed7fe93b2e4ace
crackhex <64289457+crackhex@users.noreply.github.com>
2021-04-27 11:21:14 -0700

    Add files via upload

---
+++ UI/Components/PacepalSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml;
+using System.Windows.Forms;
+
+namespace Livesplit.UI.Components
+{
+    public partial class PacepalSettings : UserControl
+    {
+        public bool UseSegmentTimes { get; set; }
+        public PacepalSettings()
+        {
+            InitializeComponent();
+
+        }
+        public void SetSettings(XmlNode node)
+        {
+            
+        }
+    }
+}
