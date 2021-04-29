549f1b1f7805c491a9dd821753291bba2a9d5752
crackhex <64289457+crackhex@users.noreply.github.com>
2021-04-29 11:05:17 -0700

    More SoB things

--- UI/Components/PacepalComponent.cs
+++ UI/Components/PacepalComponent.cs
@@ -1,18 +1,22 @@
 ﻿using LiveSplit.Model;
 using LiveSplit.TimeFormatters;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Xml;
 
+
 namespace LiveSplit.UI.Components
-{
+{ 
+    
     public class PacepalComponent : IComponent
     {
 
         public PacepalSettings Settings { get; set; }
 
+        protected InfoTimeComponent InternalComponent { get; set; }
         protected LiveSplitState State { get; set; }
         protected Form Form { get; set; }
         protected TimerModel Model { get; set; }
@@ -25,14 +29,19 @@ namespace LiveSplit.UI.Components
         public float PaddingRight => 0;
         public string ComponentName => "Pacepal Plugin";
 
+        public TimeSpan? SumOfBestValue { get; set; }
+
+        private RegularSumOfBestTimeFormatter Formatter { get; set; }
+
         public IDictionary<string, Action> ContextMenuControls { get; protected set; }
 
 
         public PacepalComponent(LiveSplitState state)
         {
+            Formatter = new RegularSumOfBestTimeFormatter();
+            InternalComponent = new InfoTimeComponent("", null, Formatter);
             Settings = new PacepalSettings();
             Model = new TimerModel();
-
             ContextMenuControls = new Dictionary<string, Action>();
             ContextMenuControls.Add("Start Pacing", Start);
 
@@ -40,14 +49,33 @@ namespace LiveSplit.UI.Components
 
         }
 
-        public void Start()
+    public void WriteToFile()
+    {
+            string folder = @"C:\Users\Path\to\repos\repos\LiveSplit\LiveSplit\Components\LiveSplit.Pacepal";
+            // Filename  
+            string fileName = "Output.txt";
+            // Fullpath. You can direct hardcode it if you like.  
+            string fullPath = folder + fileName;
+            // An array of strings  
+            string[] Sumofbest = {SumOfBestValue.ToString()};
+            File.WriteAllLines(fullPath, Sumofbest);
+
+
+    }
+
+
+        
+    public void Start()
         {
+            //Start Pacing button
             ContextMenuControls.Clear();
             ContextMenuControls.Add("Stop Pacing", Stop);
+            WriteToFile();
         }
 
         public void Stop()
         {
+            //Stop Pacing button
             ContextMenuControls.Clear();
             ContextMenuControls.Add("Start Pacing", Start);
         }
@@ -85,7 +113,10 @@ namespace LiveSplit.UI.Components
 
         public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
         {
-            
+
+            InternalComponent.TimeValue = SumOfBestValue;
+
+            InternalComponent.Update(invalidator, state, width, height, mode);
         }
 
         public void Dispose()
